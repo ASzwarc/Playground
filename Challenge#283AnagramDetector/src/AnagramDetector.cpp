2ca8348bca42afc14f1d56c8ebeb370171a27131
AdamSzwarc <szwarc.adam@gmail.com>
2016-09-26 23:05:16 +0200

    Reading input, added input file

--- Challenge#283AnagramDetector/src/AnagramDetector.cpp
+++ Challenge#283AnagramDetector/src/AnagramDetector.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
 
 int main(int argc, char *argv[])
 {
-    std::cout << "dupa" << std::endl;
+    std::string inputLine;
+    int numberOfLines = 0;
+    std::cin >> numberOfLines;
+    std::vector<std::string>input;
+    input.reserve(numberOfLines);
+    for(int i = 0; i < numberOfLines; i++)
+    {
+        std::getline(std::cin, inputLine);
+        input.push_back(inputLine);
+    }
+    for(auto line: input)
+    {
+        std::cout << line << std::endl;
+    }
     return 0;
 }
