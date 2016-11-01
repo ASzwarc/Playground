f311ad0b919e18e468acbb3258cdeee868e4cfcb
AdamSzwarc <szwarc.adam@gmail.com>
2016-11-01 21:18:02 +0100

    Created template of cmake to use in project. Building of executable works. Building of executable for tests is not created yet

---
+++ LinkedList/src/cos.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class Cos
+{
+	public:
+		Cos(int i);
+	private:
+		int someParameter_;
+};
---
+++ LinkedList/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "cos.hpp"
+
+
+int main(int argc, char* argv[])
+{
+	Cos dupa{1};
+	std::cout << "Hello World, let's check if CMake works" << std::endl;
+	return 0;
+}
