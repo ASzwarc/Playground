338d4e12bc9fd9772001dbd5ed86c247a4f4005c
aszwarc <adam.szwarc@nokia.com>
2016-11-27 21:19:43 +0100

    Initialization of new project

---
+++ Stack/src/Stack.hpp
@@ -0,0 +1,9 @@
+#ifndef STACK_HPP_
+#define STACK_HPP_
+
+class Stack
+{
+
+};
+
+#endif
---
+++ Stack/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Hello World, let's check if CMake works" << std::endl;
+	return 0;
+}
